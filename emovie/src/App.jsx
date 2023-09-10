c311ca7ff5c02bd64a66ba16d51020ccb5c36e1b
Rupesh Khadka <khadkarupesh818@gmail.com>
2023-09-10 20:39:23 +0545

    beginning

---
+++ emovie/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ emovie/src/store/store.js
@@ -0,0 +1,9 @@
+// src/app/store.js
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
