2e8ecf58b0382f6af469579d0c41321ed254956f
Rupesh Khadka <khadkarupesh818@gmail.com>
2023-09-15 17:18:06 +0545

    beginning

---
+++ emovie/backend/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require('dotenv', );
+
+
+    port=app.use
+
+// Middleware for parsing JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb://localhost/my-database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// Define your Mongoose models and schemas here
+const User = mongoose.model('User', {
+  name: String,
+  email: String,
+});
+
+// Define your routes and middleware here
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+    });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Error creating user', details: error });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
