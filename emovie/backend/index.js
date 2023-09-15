4d4ee1c04d9ad2ab74f1c5acb6d40e80fd9f4b0e
Rupesh Khadka <khadkarupesh818@gmail.com>
2023-09-16 00:33:49 +0545

    push

--- emovie/backend/index.js
+++ emovie/backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const dotenv = require('dotenv', );
+const dotenv = require('dotenv' );
 
 
     port=app.use
